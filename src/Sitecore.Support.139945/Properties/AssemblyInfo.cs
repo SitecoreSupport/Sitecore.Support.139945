cfa259ecd2853da5067c395db483785e4fdd5517
DK\ALTU <altu@sitecore.net>
2017-03-15 18:34:19 +0200

    Patch 139945 iteration 1

---
+++ src/Sitecore.Support.139945/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.139945")]
+[assembly: AssemblyProduct("Sitecore.Support.139945")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.139945/SwitchingMembershipProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Principal;
+using LightLDAP;
+using Sitecore.Security;
+using System;
+using System.Web.Security;
+
+namespace Sitecore.Support.Security
+{
+    public class SwitchingMembershipProvider : Sitecore.Security.SwitchingMembershipProvider
+    {
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            foreach (MembershipProviderWrapper wrapper in Wrappers)
+            {
+                if ((wrapper.Provider is SqlMembershipProvider && providerUserKey is Guid)
+                    || (wrapper.Provider is SitecoreADMembershipProvider && providerUserKey is SecurityIdentifier))
+                {
+                    MembershipUser user = wrapper.Provider.GetUser(providerUserKey, userIsOnline);
+                    if (user != null)
+                    {
+                        return wrapper.Globalize(user);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
